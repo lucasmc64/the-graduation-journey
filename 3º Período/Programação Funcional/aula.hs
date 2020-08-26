3fa240bfb127843a1913dc98f0a10f318ec15aaa
Lucas Marçal Coutinho <lucasmc64@pop-os.localdomain>
2020-08-26 19:38:49 -0300

    Implementação das funções vistas nas aulas 1 e 2

---
+++ 3º Período/Programação Funcional/aula.hs
@@ -0,0 +1,25 @@
+{-
+    Vídeo 2 - Introdução à Linguagem Haskell
+-}
+
+-- Soma dois valores
+soma x y = x + y
+
+-- Incrementa 1 no valor dado
+inc n = n + 1
+
+-- Calcular a media entre 3 valores
+media v1 v2 v3 = (v1 + v2 + v3) / 3
+
+-- Calcular a área de um triângulo
+area_triangulo b h = (b * h) /2
+
+-- Calcular a área de um retângulo
+area_retangulo b h = b * h
+
+-- Calcular a área de um círculo
+area_circulo r = pi * r ^ 2
+
+-- Calcular a área de um trapézio
+area_trapezio b_maior b_menor h = (b_maior + b_menor) * h / 2
+
