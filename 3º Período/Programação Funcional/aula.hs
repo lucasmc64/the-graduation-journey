ec9b8fff65d14ab3698e996e981d320d856b04fc
Lucas Marçal Coutinho <lucasmc64@pop-os.localdomain>
2020-08-26 21:07:17 -0300

    Funções implementadas na 3ª videoaula

--- 3º Período/Programação Funcional/aula.hs
+++ 3º Período/Programação Funcional/aula.hs
@@ -23,3 +23,38 @@ area_circulo r = pi * r ^ 2
 -- Calcular a área de um trapézio
 area_trapezio b_maior b_menor h = (b_maior + b_menor) * h / 2
 
+-- Função para converter um valor Fahrenheit em Celsius
+converteFC :: Float -> Float
+converteFC x = (x - 32) / 1.8
+
+-- Função para calcular a media simples de 2 números
+media_2num :: Float -> Float -> Float
+media_2num x y = (x + y) / 2
+
+-- Função que retorna o menor entre dois números inteiros
+menor :: Int -> Int -> Int
+
+{-
+menor x y
+    | x < y = x
+    | otherwise = y
+-}
+
+menor x y = if x <= y then x
+    else y
+
+-- Função que retorna o maior entre três números inteiros
+
+maxTres :: Int -> Int -> Int -> Int
+
+{-
+maxTres x y z = if x >= y && x >= z then x
+    else if y >= x && y >= z then y
+        else z
+
+-}
+
+maxTres x y z
+    | x >= y && x >= z = x
+    | y >= z = y -- x não é o maior dos 3, então só é preciso verificar em relação a z
+    | otherwise = z
