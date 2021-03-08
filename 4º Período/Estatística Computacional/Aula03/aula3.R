b4eac969f555748926da6ec50ea8d5d8aa323c12
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-03-08 19:41:50 -0300

    Update

---
+++ 4º Período/Estatística Computacional/Aula03/aula3.R
@@ -0,0 +1,39 @@
+a <- 1
+1 -> b
+
+# operações com vetores
+
+w = seq(from = 5, to = 132, by = 3)
+length(w)
+w[22]
+w[22:27]
+#falta
+
+# análise de um conjunto de dados
+
+library(datasets)
+data("iris")
+head(iris)
+tail(iris)
+str(iris)
+summary(iris)
+iris[19, 5]
+iris[, 4]
+mean(iris[, 1])
+hist(iris[, 2])
+boxplot(iris[, 3])
+sd(iris[,1])
+
+petala <- iris$Petal.Length
+setosa <- iris[iris$Species == "setosa", ]
+versicolor <- iris[iris$Species == "versicolor", ]
+virginica <- iris[iris$Species == "virginica", ]
+setosa
+
+plot(x = iris$Sepal.Length, y = iris$Petal.Length, pch = 19, main = "Sépala vs. Pétala")
+
+plot(x = iris$Sepal.Length, y = iris$Petal.Length, pch = 19, type = "n")
+points(versicolor$Sepal.Length, versicolor$Petal.Length, col = "blue", pch =19, cex = 1.5)
+points(virginica$Sepal.Lenght, virginica$Petal.Length, col = "green", pch =19, cex = 1.5)
+points(setosa$Sepal.Lenght, setosa$Petal.Length, col = "red", pch =19, cex = 1.5)
+
