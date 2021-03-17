cbea3f81492782ce2ff255578f21d3f00164cb3c
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-03-17 18:39:43 -0300

    Update

---
+++ 4-periodo/estatistica-computacional/lista-de-exercicios-2/exercicio-3.R
@@ -0,0 +1,9 @@
+# Exercício 3
+
+# Letra A
+i <- 10:100
+sum(i^3 + 4*i^2)
+
+# Letra B
+i <- 10:25
+sum((2^i)/i + (3^i)/(i^2))
---
+++ 4-periodo/estatistica-computacional/lista-de-exercicios-2/exercicio-4.R
@@ -0,0 +1,16 @@
+# Exercício 4
+
+xVec <- sample(0:999, 250, replace=T)
+yVec <- sample(0:999, 250, replace=T)
+
+# Letra A
+xVecImpares <- xVec[xVec ]
+odd(4)
+
+# Letra B
+
+# Letra C
+
+# Letra D
+
+# Letra E
