6645101f342e374d523acc6f587981220e7d272e
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-04-07 21:51:07 -0300

    Update

--- 4-periodo/sistemas-de-bancos-de-dados/sec-exemplos.sql
+++ 4-periodo/sistemas-de-bancos-de-dados/sec-exemplos.sql
@@ -8,6 +8,7 @@ SET SEARCH_PATH TO sec; -- DEFINE ESQUEMA DEFAULT
 
 INSERT INTO responsavel (idResponsavel) VALUES (135795793000015);
 INSERT INTO responsavel (idResponsavel) VALUES (12345678901);
+INSERT INTO responsavel (idResponsavel) VALUES (98765432101);
 
 -- Empresas
 
@@ -36,6 +37,7 @@ INSERT INTO prestadora (idEmpresa, idResponsavel) VALUES (4, 135795793000015);
 -- Pessoas
 
 INSERT INTO pessoa (cpf, nomePessoa, dataPagamentoInscricao, tipoPessoa, idEmpresa, idResponsavel) VALUES (12345678901, 'Marília Medonça', now(), 'EP', 1, 12345678901);
+INSERT INTO pessoa (cpf, nomePessoa, dataPagamentoInscricao, tipoPessoa, idEmpresa, idResponsavel) VALUES (98765432101, 'Paula Fernandes', now(), 'PR', 2, 98765432101);
 
 -- Serviços
 
@@ -44,5 +46,13 @@ INSERT INTO servico (descricaoServico, custoServico, estadoServico, dataTerminoS
 INSERT INTO servico (descricaoServico, custoServico, estadoServico, dataTerminoServico, idResponsavel) VALUES ('Fazer chover', 14752, 'E', now(), 135795793000015);
 INSERT INTO servico (descricaoServico, custoServico, estadoServico, dataTerminoServico, idResponsavel) VALUES ('Vender sapos de chocolate', 1876, 'G', now(), 12345678901);
 
+-- Tarefa
+
+INSERT INTO tarefa (descricaoTarefa, idPessoa, idSuperTarefa) VALUES ('Refazer o site da Tesla', 1, NULL);
+INSERT INTO tarefa (descricaoTarefa, idPessoa, idSuperTarefa) VALUES ('Criar SVG e animá-los', 1, 1);
+
+-- Inscrição
+
+INSERT INTO inscricao (idPessoa, idTarefa) VALUES (2, 1);
 
 END TRANSACTION;
