77a24cfb520cf763068d76070a5013c87d1f2cb8
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-05-22 20:40:18 -0300

    Update

--- 4-periodo/sistemas-de-bancos-de-dados/flj-part-2.sql
+++ 4-periodo/sistemas-de-bancos-de-dados/flj-part-2.sql
@@ -1,14 +1,69 @@
 -- 2
 
+-- -- II
+
+-- -- -- 1
+
+SELECT loja.idloja, nome, quatidade
+  FROM produto, estoque, loja
+  WHERE 
+    produto.idprod = estoque.idprod
+    AND
+    loja.idloja = estoque.idloja
+    AND
+    quantidade > 0
+
+-- -- -- 2
+
+SELECT produto.idprod, nome, COUNT(quantidade) AS qtd_vendas
+FROM venda_fisica, produto
+WHERE
+  venda_fisica.idprod = produto.idprod
+GROUP BY produto.idprod, nome
+ORDER BY nome ASC
+
+-- -- -- 3
+
+SELECT produto.idprod, nome
+FROM venda_online, produto
+WHERE
+  venda_online.idprod = produto.idprod
+  AND
+  preco >= ALL (
+    SELECT preco
+    FROM venda_online, produto
+    WHERE
+      venda_online.idprod = produto.idprod
+  )
+
+-- -- III
+
+-- -- -- 1
+
+SELECT cadastrocliente, SUM(quantidade * preco) AS valor_gasto_por_cliente
+FROM venda_fisica NATURAL JOIN produto
+GROUP BY cadastrocliente
+
+-- -- -- 2
+
+SELECT loja.idloja, COUNT(*) AS num_funcionarios
+FROM loja LEFT OUTER JOIN funcionario ON loja.idloja = funcionario.idloja
+GROUP BY loja.idloja
+
+-- -- -- 3
+
+SELECT AVG(quantidade * preco) AS gasto_medio_geral
+FROM venda_fisica NATURAL JOIN produto, venda_online NATURAL JOIN produto 
+
 -- -- IV
 
-WITH RECURSIVE nome () AS (
+WITH RECURSIVE hierarquiaFunc (nivel, idloja, idfun, nomepes, funcao) AS (
   UNION ALL
 )
 
-SELECT DISTINCT 
-FROM nome
-ORDER BY ;
+SELECT DISTINCT nivel, idloja, idfun, nomepes, funcao
+FROM hierarquiaFunc
+ORDER BY nivel, idloja, idfun;
 
 -- -- VII -> View que informa os nomes dos produtos disponíveis e suas quantidades, sem expor os ids dos produtos ou da respectiva loja a que pertence, além de restingir os dados aos de uma única loja
 
